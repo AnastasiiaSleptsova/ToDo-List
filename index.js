571976351a043db7e2dc7a1e5080bef358b07704
AnastasiiaSleptsova <am89650731404@gmail.com>
2024-07-01 11:43:36 +0300

    add settings project

---
+++ index.js
@@ -0,0 +1,16 @@
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
+
+const app = () => {
+    
+}
+
+app();
+
+// import axios from 'axios';
+
+// const routes = {
+//   tasksPath: () => '/api/tasks',
+// };
+
+
